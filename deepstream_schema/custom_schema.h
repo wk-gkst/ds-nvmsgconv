8917f311b96c1b33850af7881bd6888f41ffe5d1
william khoo <williamkhoo@gkst.com.my>
2022-08-13 11:43:45 +0800

    no msg bug fix

--- deepstream_schema/custom_schema.h
+++ deepstream_schema/custom_schema.h
@@ -1,16 +1,30 @@
 
+#ifndef NVCUSTOMMSGCONV_H_
+#define NVCUSTOMMSGCONV_H_
+
 #include <string>
+#include <glib.h>
 
 using namespace std;
 
-struct NvDsPPEObject
+#ifdef __cplusplus
+extern "C"
 {
-    gchar* stream_id;
-    gchar* frame_unique_id;
-    gchar* frame;
-    int hasVest;
-    int hasHelm;
-    int full;
-    gchar* lcStatus;
-    gchar* direction;
-};
+#endif
+
+typedef struct NvDsPPEObject
+{
+    gchar *stream_id;
+    gchar *frame_unique_id;
+    gchar *frame;
+    guint hasVest;
+    guint hasHelm;
+    guint full;
+    gchar *lcStatus;
+    gchar *direction;
+} NvDsPPEObject;
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* NVCUSTOMMSGCONV_H_ */
